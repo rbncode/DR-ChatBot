ac2ebaac2c03ec9ff94e44ffce370ae523acbdcd
FranUFRO <f.neira04@ufromail.cl>
2025-04-28 12:04:09 -0400

    detalles endpoint

--- backend/bot.py
+++ backend/bot.py
@@ -8,19 +8,18 @@ import re
 # Crear una instancia de FastAPI
 app = FastAPI()
 
-# Función para consultar Gemini AI
 def consultar_gemini(pregunta):
     try:
         load_dotenv("key.env") 
-        api_key = os.getenv("GEMINI_API_KEY")
+        api_key = os.getenv("GEMMA_API_KEY")
         
         if not api_key:
-            raise HTTPException(status_code=500, detail="Error: No se encontró la clave API de Gemini.")
+            raise HTTPException(status_code=500, detail="Error: No se encontró la clave API de gemma.")
         
         # Configurar la API de Gemini
         genai.configure(api_key=api_key)
         
-        # Seleccion de modelos disponibles en tu lista
+        # Seleccion del modelo
         modelo = genai.GenerativeModel('gemma-3-27b-it')
         
         # Enviar la consulta y obtener respuesta
@@ -29,12 +28,11 @@ def consultar_gemini(pregunta):
         return respuesta.text
         
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error al consultar Gemini AI: {str(e)}")
+        raise HTTPException(status_code=500, detail=f"Error al consultar gemma: {str(e)}")
 
-# Función para realizar búsqueda en Google
 def busqueda_google_chatbot(pregunta):
     try:
-        genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+        genai.configure(api_key=os.getenv("GEMMA_API_KEY"))
         model = genai.GenerativeModel('gemma-3-27b-it')
         
         params = {
@@ -50,28 +48,27 @@ def busqueda_google_chatbot(pregunta):
         # Consulta con contexto
         respuesta = model.generate_content(f"Con base en esta información:\n{contexto}\n\n {pregunta}")
 
-        return respuesta.text  # Devolver solo el texto de la respuesta
+        return respuesta.text
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error al realizar la búsqueda en Google: {str(e)}")
 
-# Endpoint principal
+# endpoint
 @app.post("/chatbot/")
 async def chatbot_endpoint(pregunta_usuario: dict):
     try:
         # Cargar variables de entorno
         load_dotenv("key.env") 
-
     
-        system_prompt = "En base a la pregunta del usuario se debe identificar una busqueda en google a realizar clave, que puede ser solo una de la siguiente lista " \
-        "1) El clima del dia en la zona que se pregunta el dia de hoy(esta debe ser respondida con respecto a como se pregunte) " \
+        system_promt = "En base a la pregunta del usuario se debe identificar una busqueda en google a realizar clave, que puede ser solo una de la siguiente lista " \
+        "1) El clima del dia en la zona que se pregunta el dia de hoy(esta debe ser respondida con respecto a como se pregunte) si no se identifica la zona en la pregunta devolver: Lo siento, falta información para poder contestar a su consulta" \
         "2) El valor de la UF a CLP hoy" \
-        "3) Valo del dolar a CLP hoy" \
+        "3) Valo del dolar a CLP hoy y solo puede ser en estas monedas, si se pregunta de otra moneda no corresponde" \
         "4) Noticias del dia en la zona que se pregunta y si no se especifica entonces de manera global" \
-        "5) La hora actual en la zona que se pregunta" \
-        "Si el usuario pregunta algo fuera de esto devuelve -NEGATIVO- " \
+        "Si el usuario pregunta algo fuera de esto devuelve una disculpa y aclarar que no puedes contestar" \
         "Si puedes identificar la pregunta en una de las catergoria responde como si fueras a realizar una busqueda en google del tema poniendo lo que buscaras " \
         "entre 2 #." \
+        "Si se identifica un saludo o despedida de parte del usuario responde como si fueras un asistente virtual y no como un buscador de google correspondiendo al saludo" \
         "La pregunta del usuario es : "
 
         # Obtener la pregunta del usuario desde el JSON enviado
